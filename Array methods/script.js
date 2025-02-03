d0a4811d20f0bfb05289718967ee4877198fca3d
doni.ac <132898784+Doniyor6444@users.noreply.github.com>
2025-02-03 10:26:49 +0500

    Add files via upload

---
+++ Array methods/script.js
@@ -0,0 +1,19 @@
+let tasks = [];
+
+function renderTasks() {
+  document.getElementById("todoList").innerHTML = tasks
+    .map(
+      (task, i) =>
+        `<li>${task} <button onclick="tasks.splice(${i}, 1); renderTasks()">O'chirish</button></li>`
+    )
+    .join("");
+}
+
+function addTask() {
+  let input = document.getElementById("taskInput");
+  if (input.value.trim()) {
+    tasks.push(input.value);
+    input.value = "";
+    renderTasks();
+  }
+}
